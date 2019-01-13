7628ccce7e0bc6fcc9711183d3b9e1a78a1d9a74
chownrootroot <chownrootroot@tutanota.com>
2019-01-13 14:45:01 +0100

    Work in progress

--- wlgen.py
+++ wlgen.py
@@ -2,17 +2,74 @@
 # Wordlist generator
 # ------------------------------
 
+strFirstName = ""
+strLastName = ""
+strYear = ""
+strMonth = ""
+strDay = ""
+strPartnerFirstName = ""
+strPartnerLastName = ""
+strPartnerYear = ""
+strPartnerMonth = ""
+strPartnerDay = ""
+strChild1FirstName = ""
+strChild1LastName = ""
+strChild1Year = ""
+strChild1Month = ""
+strChild1Day = ""
+strChild2FirstName = ""
+strChild2LastName = ""
+strChild2Year = ""
+strChild2Month = ""
+strChild2Day = ""
+strChild3FirstName = ""
+strChild3LastName = ""
+strChild3Year = ""
+strChild3Month = ""
+strChild3Day = ""
+
 # Get input from user
-strFirstName = 			raw_input("Subjects first name: ")
-strLastName = 			raw_input("Subjects last name: ")
-strYear = 				raw_input("Subjects year of birth: ")
-strMonth = 				raw_input("Subjects month of birth: ")
-strDay = 				raw_input("Subjects day of birth: ")
-strPartnerFirstName = 	raw_input("Subjects partners first name: ")
-strPartnerLastName = 	raw_input("Subjects partners last name: ")
-strPartnerYear = 		raw_input("Subjects partners year of birth: ")
-strPartnerMonth = 		raw_input("Subjects partners month of birth: ")
-strPartnerDay = 		raw_input("Subjects partners day of birth: ")
+strFirstName = raw_input("Subjects first name: ")
+strLastName = raw_input("Subjects last name: ")
+strYear = raw_input("Subjects year of birth: ")
+strMonth = raw_input("Subjects month of birth: ")
+strDay = raw_input("Subjects day of birth: ")
+
+strAnswer = "n"
+strAnswer = raw_input("Partner? <y/n>: ")
+if strAnswer.lower() == "y": 
+	strPartnerFirstName = raw_input("Partners first name: ")
+	strPartnerLastName = raw_input("Partners last name: ")
+	strPartnerYear = raw_input("Partners year of birth: ")
+	strPartnerMonth = raw_input("Partners month of birth: ")
+	strPartnerDay = raw_input("Partners day of birth: ")
+
+strAnswer = "n"
+strAnswer = raw_input("Child? <y/n>: ")
+if strAnswer.lower() == "y": 
+	strChild1FirstName = raw_input("Childs first name: ")
+	strChild1LastName = raw_input("Childs last name: ")
+	strChild1Year = raw_input("Childs year of birth: ")
+	strChild1Month = raw_input("Childs month of birth: ")
+	strChild1Day = raw_input("Childs day of birth: ")
+
+	strAnswer = "n"
+	strAnswer = raw_input("Another child? <y/n>: ")
+	if strAnswer.lower() == "y": 
+		strChild2FirstName = raw_input("Childs first name: ")
+		strChild2LastName = raw_input("Childs last name: ")
+		strChild2Year = raw_input("Childs year of birth: ")
+		strChild2Month = raw_input("Childs month of birth: ")
+		strChild2Day = raw_input("Childs day of birth: ")
+
+		strAnswer = "n"
+		strAnswer = raw_input("Another child? <y/n>: ")
+		if strAnswer.lower() == "y": 
+			strChild3FirstName = raw_input("Childs first name: ")
+			strChild3LastName = raw_input("Childs last name: ")
+			strChild3Year = raw_input("Childs year of birth: ")
+			strChild3Month = raw_input("Childs month of birth: ")
+			strChild3Day = raw_input("Childs day of birth: ")
 
 # ------------------------------
 # Add to names wordlist
@@ -32,6 +89,18 @@ if strPartnerFirstName != "":
 	lstrNames.append(strPartnerFirstName)
 if strPartnerLastName != "":
 	lstrNames.append(strPartnerLastName)
+if strChild1FirstName != "":
+	lstrNames.append(strChild1FirstName)
+if strChild1LastName != "":
+	lstrNames.append(strChild1LastName)
+if strChild2FirstName != "":
+	lstrNames.append(strChild2FirstName)
+if strChild2LastName != "":
+	lstrNames.append(strChild2LastName)
+if strChild3FirstName != "":
+	lstrNames.append(strChild3FirstName)
+if strChild3LastName != "":
+	lstrNames.append(strChild3LastName)
 
 # Add combinations
 tempNames1 = lstrNames
@@ -66,6 +135,24 @@ if strPartnerMonth != "":
 	lstrDates.append(strPartnerMonth)
 if strPartnerDay != "":
 	lstrDates.append(strPartnerDay)
+if strChild1Year != "":
+	lstrDates.append(strChild1Year)
+if strChild1Month != "":
+	lstrDates.append(strChild1Month)
+if strChild1Day != "":
+	lstrDates.append(strChild1Day)
+if strChild2Year != "":
+	lstrDates.append(strChild2Year)
+if strChild2Month != "":
+	lstrDates.append(strChild2Month)
+if strChild2Day != "":
+	lstrDates.append(strChild2Day)
+if strChild3Year != "":
+	lstrDates.append(strChild3Year)
+if strChild3Month != "":
+	lstrDates.append(strChild3Month)
+if strChild3Day != "":
+	lstrDates.append(strChild3Day)
 
 # Add combinations
 tempDates1 = lstrDates
@@ -86,6 +173,12 @@ lstrMain = []
 # Combine dates and names
 for x in lstrNames:
 	for y in lstrDates:
+		lstrMain.append(x)
+		lstrMain.append(x.upper())
+		lstrMain.append(x.lower())
+		lstrMain.append(y)
+		lstrMain.append(y.upper())
+		lstrMain.append(y.lower())
 		lstrMain.append(y + x)
 		lstrMain.append(y + x.upper())
 		lstrMain.append(y + x.lower())
@@ -95,7 +188,7 @@ for x in lstrNames:
 # ------------------------------
 # Write to file 
 # ------------------------------
-file = open("wordlist.txt","w")
+file = open(strFirstName + "_wordlist.txt","w")
 for x in lstrMain:
 	file.write(x + '\n')
 file.close()
